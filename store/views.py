e840f92fd1d52c93afbe1f16147f9b8070f1658c
Pravin Kalokhe <pravinkalokhe5@gmail.com>
2024-06-24 13:07:57 +0530

    Form Signuo Create

--- store/views.py
+++ store/views.py
@@ -3,10 +3,20 @@ from .models.product import Product
 from .models.category import Category
 # Create your views here.
 def index(request):
-    product=Product.get_all_products();
+    product=None
     category=Category.get_all_categories();
+    categoryID=request.GET.get('category')
+    if categoryID:
+        product=Product.get_all_products_by_categoryid(categoryID);
+    else:
+        product=Product.get_all_products();
     data={}
     data['product']=product
     data['category']=category
     return render(request,'index.html',data)
     # return render (request,'orders/orders.html')
+
+
+
+def signup(request):
+    return render (request,'signup.html')
